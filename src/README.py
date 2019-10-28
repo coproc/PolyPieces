30c6f97e149a58096f83bd4e53d1dbcdb678ca24
coproc <wolfgang@stoecher.com>
2019-10-28 22:08:33 +0100

    MAK update python commands from README

--- src/README.py
+++ src/README.py
@@ -1,10 +1,14 @@
+# PolyPieces README
 from UniVarPoly import UniVarPoly, p_x as x
 poly = 3*x - 1
 poly*poly
 from PolyPieces import PolyPiece, PolyPieceFunc
-uniformRandomDensity = PolyPieceFunc([PolyPiece([0,1], UniVarPoly(1))])
-print(uniformRandomDensity)
-uniformRandomDensitySum2 = uniformRandomDensity.conv(uniformRandomDensity)
-print(uniformRandomDensitySum2)
-uniformRandomDensitySum3 = uniformRandomDensitySum2.conv(uniformRandomDensity)
-print(uniformRandomDensitySum3)
+# define density for uniform distribution over the interval [0,1]
+uniformDensity = PolyPieceFunc(PolyPiece(1, [0,1]))
+print(uniformDensity)
+# compute density of the sum of two uniformly distributed random variables (by convolution)
+uniformDensitySum2 = uniformDensity.conv(uniformDensity)
+print(uniformDensitySum2)
+# compute density of the sum of three uniformly distributed random variables
+uniformDensitySum3 = uniformDensitySum2.conv(uniformDensity)
+print(uniformDensitySum3)

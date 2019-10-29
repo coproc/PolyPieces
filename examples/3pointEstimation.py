07e8019fb937fa313fd3ef95529ff2a93c7452bf
coproc <wolfgang@stoecher.com>
2019-10-29 15:52:53 +0100

    FIX adjust construction of PolyPiece-s ADD ascii plot of density functions

--- examples/3pointEstimation.py
+++ examples/3pointEstimation.py
@@ -3,31 +3,60 @@
 import sys
 sys.path.append('../src')
 
-from UniVarPoly import UniVarPoly
+from UniVarPoly import UniVarPoly, p_x as x
 from PolyPieces import PolyPiece, PolyPieceFunc
+
 
 # generate density function of triangular distribution
 def triangDistDensity(a,b,c):
-	pp1 = PolyPiece([a,c], UniVarPoly([0, 2./(b-a)/(c-a)]).comp([-a,1]))
-	pp2 = PolyPiece([c,b], UniVarPoly([0,-2./(b-a)/(b-c)]).comp([-b,1]))
-	return PolyPieceFunc([pp1,pp2])
+	pp1 = PolyPiece((x-a)/(c-a), [a,c])
+	pp2 = PolyPiece((b-x)/(b-c), [c,b])
+	return PolyPieceFunc([pp1,pp2])*2/(b-a)
 
+
+PLOT_DOTS = ('\N{COMBINING DOT BELOW}', '.', '\N{MIDDLE DOT}', '\N{DOT ABOVE}')
+# print((len(PLOT_DOTS)+1)*' '+PLOT_DOTS[0])
+# print('_'+''.join(PLOT_DOTS)+'+')
+# print(PLOT_DOTS[-1])
+
+def asciiPlot(f, xRange, yRange=None, xRes=80, yRes=20, unicodeOutput=True):
+	dx = (xRange[1]-xRange[0])/xRes
+	xVals = [xRange[0]+i*dx for i in range(xRes+1)]
+	yVals = [f(x) for x in xVals]
+	yMin,yMax = (min(yVals),max(yVals)) if yRange is None else yRange
+	print(yMin,yMax)
+	dy = (yMax-yMin)/(yRes-1)
+	plotArea = [len(xVals)*' ' for _ in range(yRes+1)]
+	for xIdx,y in enumerate(yVals):
+		yScaled = (yMax-y)/dy+0.4
+		yIdx = int(yScaled)
+		line = plotArea[yIdx]
+		dotChar = PLOT_DOTS[3-int(3.98*(yScaled-yIdx)+0.01)] if unicodeOutput else '.'
+		plotArea[yIdx] = line[0:xIdx] + dotChar + line[xIdx+1:]
+	return '\n'.join(plotArea)
+
+def plotFpp(fpp, xRange=None, yRange=None, xRes=80, yRes=20, unicodeOutput=True):
+	polyPieces = fpp.polyPieces
+	if xRange is None: xRange = [polyPieces[0].interval[0],polyPieces[-1].interval[1]]
+	return asciiPlot(lambda x: fpp.eval(x), xRange, yRange, xRes, yRes, unicodeOutput)
+	
 
 from random import randint
 
-for i in range(10):
-	# 
+triangleDists = []
+for i in range(4):
 	a = randint(1,10)
 	c = a + randint(1,3)
-	b = c + randint(1,5)
-	density3point = triangDistDensity(a,b,c)
+	b = c + randint(1,7)
+	triangleDists.append(triangDistDensity(a,b,c))
 
-	if i == 0:
-		densityOfSum = density3point
-	else:
-		densityOfSum %= density3point
-	print('.', end = '') # show progress
+for td in triangleDists:
+	print(plotFpp(td, [0,20], [0,1], yRes=8))
 
-print()
-print('density of sum of random variables with triangular distribution')
+from functools import reduce
+densityOfSum = reduce(lambda x,y: x^y, triangleDists)
+
+print('\ndensity of sum of random variables with triangular distribution')
 print(densityOfSum)
+print()
+print(plotFpp(densityOfSum))

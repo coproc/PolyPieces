8e4fb7d0e80e771ed18b146ec85ca02db60ea4a1
coproc <wolfgang@stoecher.com>
2019-10-29 17:20:46 +0100

    TST completely rewrite and extend approximation of normal distribution

--- examples/approxNormalDist.py
+++ examples/approxNormalDist.py
@@ -1,53 +1,79 @@
 '''generate a piecewise polynomial function approximating Gauss' bell curve
-   by convoluting the density of a uniform random variable with itself several times
+   by convoluting the density of a uniform random variable with itself several times.
+   We do exact computations by using coefficients of type fractions.Fraction.
 '''
 import sys
 sys.path.append('../src')
 
 import math
+from fractions import Fraction
 from UniVarPoly import UniVarPoly, p_x, p_x2
 from PolyPieces import PolyPiece, PolyPieceFunc
+import AsciiPlot
 
 
-# compute variance of a symmetric density function (i.e. expectation value is 0)
-def varSym(fpp):
+def normalDistDensity(expVal=0, sigma=1):
+	normFac = 1/math.sqrt(2*math.pi)/sigma
+	return lambda x: normFac*math.exp(-0.5*((x-expVal)/sigma)**2)
+
+
+# compute expectation value
+# Note: E(X) = integral(-inf,inf) xd(x)dx,
+#   where d(x) is the density function of random variable X.
+# @param fpp density function as piecewise polynomial function
+def expectation(fpp):
+	fpp_x = p_x * fpp
+	return fpp_x.int()
+
+
+# compute variance
+# Note: V(X) = E(X^2) - E(X)^2, 
+#   where E(g(X)) = integral(-inf,inf) g(x)d(x)dx and
+#   d(x) is the density function of random variable X.
+# @param fpp density function as piecewise polynomial function
+def variance(fpp, expVal=None):
+	if expVal is None: expVal = expectation(fpp)
 	fpp_x2 = p_x2 * fpp
-	return fpp_x2.int()
+	return fpp_x2.int() - expVal*expVal
 
 
-def normedDist(fDist):
-	area, var = fDist.int(), varSym(fDist)
-	print("area", area, "var", var)
-	fDistNormed1 = fDist.comp(UniVarPoly([0, math.sqrt(var)]))
-	area1, var1 = fDistNormed1.int(), varSym(fDistNormed1)
-	print("area1", area1, "var1", var1)
-	fDistNormed2 = 1./area1 * fDistNormed1
-	area2, var2 = fDistNormed2.int(), varSym(fDistNormed2)
-	print("area2", area2, "var2", var2)
-	return fDistNormed2
+d_deg0 = PolyPieceFunc(PolyPiece(Fraction(1), [0,1]))
+d_deg1 = d_deg0^d_deg0 # polynomials in d_deg1 have degree 1
+d_deg3 = d_deg1^d_deg1 # polynomials in d_deg3 have degree 3
 
-	
-rg = 1 # 2*math.sqrt(3)
-fDist_deg0 = PolyPieceFunc([PolyPiece([-rg/2.,rg/2.], UniVarPoly(1./rg))])
-print(fDist_deg0)
-print(fDist_deg0.int(), varSym(fDist_deg0))
+print("approximation of normal distribution of degree 3:")
+print(d_deg3)
+print()
+print(AsciiPlot.plotFpp(d_deg3))
+expVal = expectation(d_deg3)
+var = variance(d_deg3)
+d_deg3_max = d_deg3.eval(2)
+print("area=%s, expectation value=%s, variance=%s, f_max=f(2)=%s=%f" % (d_deg3.int(), expVal, var, d_deg3_max, d_deg3_max))
 
-fDist_deg1 = fDist_deg0.conv(fDist_deg0)
-fDist_deg2 = convUnitVar(fDist_deg1, fDist_deg0)
-fDist_deg4 = convUnitVar(fDist_deg2, fDist_deg1)
+ndd = normalDistDensity(expVal, math.sqrt(var))
+#print(AsciiPlot.plot(ndd, [0,4]))
+ndd_dev = lambda x: d_deg3.eval(x) - ndd(x)
+print("\ndeviation from corresponding normal distribution:")
+print(AsciiPlot.plot(ndd_dev, [0,4]))
+devVals = [ndd_dev(4*i/100) for i in range(100)]
+print("min=%f, max=%f, dev at 2=%.1f%%" % (min(devVals),max(devVals),100*ndd_dev(2)/d_deg3_max))
 
-#print('fDist_deg1:')
-#print(fpp1)
-#print(fpp1.int(), varSym(fpp1))
-#print('fppConv2:')
-#print(fpp2)
-#print(fpp2.int(), varSym(fpp2))
-#print('fppConv4:')
-#print(fpp4)
-#print(fpp4.int(), varSym(fpp4))
-#print(list(map(varSym, [fpp0, fpp1, fpp2, fpp4])))
-#print(fpp4._isContinuous(prec=1e-5, printFailReason=True))
 
-fDist_deg4_normed = normedDist(fDist_deg4)
-print('approximation of normal density of degree 4')
-print(fDist_deg4_normed)
+d_deg7 = d_deg3^d_deg3
+d_deg11 = d_deg7^d_deg3
+d_deg11_s = d_deg11.comp(p_x+Fraction(6))
+print("\napproximation of standard normal distribution of degree 11:")
+print(d_deg11_s)
+print()
+#print(AsciiPlot.plotFpp(d_deg3))
+expVal_s = expectation(d_deg11_s)
+var_s = variance(d_deg11_s)
+d_deg11_max = d_deg11_s.eval(0)
+print("area=%s, expectation value=%s, variance=%s, f_max=f(0)=%s=%f" % (d_deg11_s.int(), expVal_s, var_s, d_deg11_max, d_deg11_max))
+
+ndd_s = normalDistDensity()
+ndd_s_dev = lambda x: d_deg11_s.eval(x) - ndd_s(x)
+print("\ndeviation from standard normal distribution:")
+print(AsciiPlot.plot(ndd_s_dev, [-6,6]))
+devsVals = [ndd_s_dev(12*i/100-6) for i in range(100)]
+print("min=%f, max=%f, dev at 0=%.1f%%" % (min(devsVals),max(devsVals),100*ndd_s_dev(0)/d_deg11_max))

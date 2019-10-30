320a1ff686492fafb297e0d84ed4660c857e5e3d
coproc <wolfgang@stoecher.com>
2019-10-30 16:41:42 +0100

    TST minor improvements for testing python commands in README

--- examples/README_commands.py
+++ examples/README_commands.py
@@ -1,23 +1,31 @@
-import sys
-sys.path.append("../src")
-_ = None
+
+#------------ CODE INIT ------------
+# make sure imports from ../src work
+import os, sys
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(FILE_DIR, os.pardir, 'src'))
+# simulate console output of expressions
+_p_ = None
+# print result of last expression
+def _p():
+	global _p_
+	if _p_ is not None:
+		print(_p_.__repr__())
+		_p_ = None
+#------------ CODE INIT ------------
+
 from UniVarPoly import UniVarPoly, p_x as x
-_ = None
-if _ is not None: print(_.__repr__())
-_ = poly = 3*x - 1
-_ = poly*poly
-if _ is not None: print(_.__repr__())
+poly = 3*x - 1
+_p_= poly*poly
+_p()
 from PolyPieces import PolyPiece, PolyPieceFunc
-_ = None
-if _ is not None: print(_.__repr__())
 # define density for uniform distribution over the interval [0,1]
-_ = uniformDensity = PolyPieceFunc(PolyPiece(1, [0,1]))
-_ = print(uniformDensity)
+uniformDensity = PolyPieceFunc(PolyPiece(1, [0,1]))
+print(uniformDensity)
 # compute density of the sum of two uniformly distributed random variables (by convolution)
-_ = uniformDensitySum2 = uniformDensity.conv(uniformDensity)
-_ = print(uniformDensitySum2)
+uniformDensitySum2 = uniformDensity.conv(uniformDensity)
+print(uniformDensitySum2)
 from fractions import Fraction
-_ = None
-_ = poly_rat = Fraction(5)*x - Fraction(1,4)
-_ = print(poly_rat)
-_ = print(poly_rat.int())
+poly_rat = Fraction(5)*x - Fraction(1,4)
+print(poly_rat)
+print(poly_rat.int())
--- examples/extractPythonCodeFromREADME.py
+++ examples/extractPythonCodeFromREADME.py
@@ -1,38 +1,65 @@
-import os
+import os, re
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 README_PATH = os.path.join(FILE_DIR, os.pardir, 'README.md')
 README_CODE_PATH = os.path.join(FILE_DIR, 'README_commands.py')
 
+CODE_INIT = '''
+#------------ CODE INIT ------------
+# make sure imports from ../src work
+import os, sys
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(FILE_DIR, os.pardir, 'src'))
+# simulate console output of expressions
+_p_ = None
+# print result of last expression
+def _p():
+	global _p_
+	if _p_ is not None:
+		print(_p_.__repr__())
+		_p_ = None
+#------------ CODE INIT ------------
+
+'''
+
 insideCode = False
 codeType = None
 linePrev = None
 
+
+def _isExpression(line):
+	STATEMENT_STARTS = ['def ', 'import ', 'from ', 'print(']
+	line = line.strip()
+	for start in STATEMENT_STARTS:
+		if line.startswith(start): return False
+	# assignment?
+	if re.match(r'^[A-Za-z0-9_]+[ \t]*=.*', line): return False
+	return len(line) > 0
+
+	
 with open(README_PATH) as fileIn:
 	with open(README_CODE_PATH, 'w') as fileOut:
-		fileOut.write('import sys\n')
-		fileOut.write('sys.path.append("../src")\n')
-		fileOut.write('_ = None\n')
+		fileOut.write(CODE_INIT)
 		for line in fileIn:
 			if line.startswith('```'):
 				insideCode = not insideCode
 				if insideCode:
 					codeType = line[3:].strip()
 				else:
+					if linePrev and linePrev.startswith('>>> ') and _isExpression(linePrev[4:]):
+						print("linePrev='%s'" % linePrev)
+						fileOut.write('_p()\n')
 					codeType = None
+					linePrev = None
 				continue
 			if insideCode and codeType == 'python':
 				if line.startswith('>>> '):
 					command = line[4:]
-					SPECIAL_STARTS = ['def ', 'import ', 'from ']
-					for start in SPECIAL_STARTS:
-						if command.strip().startswith(start):
-							command += '_ = None\n'
-							break
-					else: command = '_ = ' + command
+					if _isExpression(command):
+						command = '_p_= ' + command
 					fileOut.write(command)
 				elif line.startswith('#'):
 					fileOut.write(line)
-				elif linePrev and not linePrev[4:].startswith('print(') and not ' = ' in linePrev:
-					fileOut.write('if _ is not None: print(_.__repr__())\n')
+				elif len(line.strip()) > 0 and linePrev and _isExpression(linePrev[4:]):
+					fileOut.write('_p()\n')
 				linePrev = line

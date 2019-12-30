0dea72b7a42c3e8f187b22362e83f1b7540a6c0f
coproc <wolfgang@stoecher.com>
2019-12-30 19:10:04 +0100

    REF rename AsciiPlot to TextPlot

--- examples/approxNormalDist.py
+++ examples/approxNormalDist.py
@@ -9,7 +9,7 @@ import math
 from fractions import Fraction
 from UniVarPoly import UniVarPoly, p_x, p_x2
 from PolyPieces import PolyPiece, PolyPieceFunc
-import AsciiPlot
+import TextPlot
 
 
 def normalDistDensity(expVal=0, sigma=1):
@@ -44,17 +44,17 @@ d_deg3 = d_deg1^d_deg1 # polynomials in d_deg3 have degree 3
 print("approximation of normal distribution of degree 3:")
 print(d_deg3)
 print()
-print(AsciiPlot.plotFpp(d_deg3))
+print(TextPlot.plotFpp(d_deg3))
 expVal = expectation(d_deg3)
 var = variance(d_deg3)
 d_deg3_max = d_deg3.eval(2)
 print("area=%s, expectation value=%s, variance=%s, f_max=f(2)=%s=%f" % (d_deg3.int(), expVal, var, d_deg3_max, d_deg3_max))
 
 ndd = normalDistDensity(expVal, math.sqrt(var))
-#print(AsciiPlot.plot(ndd, [0,4]))
+#print(TextPlot.plot(ndd, [0,4]))
 ndd_dev = lambda x: d_deg3.eval(x) - ndd(x)
 print("\ndeviation from corresponding normal distribution:")
-print(AsciiPlot.plot(ndd_dev, [0,4]))
+print(TextPlot.plot(ndd_dev, [0,4]))
 devVals = [ndd_dev(4*i/100) for i in range(100)]
 print("min=%f, max=%f, dev at 2=%.1f%%" % (min(devVals),max(devVals),100*ndd_dev(2)/d_deg3_max))
 
@@ -65,7 +65,7 @@ d_deg11_s = d_deg11.comp(p_x+Fraction(6))
 print("\napproximation of standard normal distribution of degree 11:")
 print(d_deg11_s)
 print()
-#print(AsciiPlot.plotFpp(d_deg3))
+#print(TextPlot.plotFpp(d_deg3))
 expVal_s = expectation(d_deg11_s)
 var_s = variance(d_deg11_s)
 d_deg11_max = d_deg11_s.eval(0)
@@ -74,6 +74,6 @@ print("area=%s, expectation value=%s, variance=%s, f_max=f(0)=%s=%f" % (d_deg11_
 ndd_s = normalDistDensity()
 ndd_s_dev = lambda x: d_deg11_s.eval(x) - ndd_s(x)
 print("\ndeviation from standard normal distribution:")
-print(AsciiPlot.plot(ndd_s_dev, [-6,6]))
+print(TextPlot.plot(ndd_s_dev, [-6,6]))
 devsVals = [ndd_s_dev(12*i/100-6) for i in range(100)]
 print("min=%f, max=%f, dev at 0=%.1f%%" % (min(devsVals),max(devsVals),100*ndd_s_dev(0)/d_deg11_max))

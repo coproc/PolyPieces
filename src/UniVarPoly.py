coproc/PolyPieces

commit 6d00a4070490c7c64243a5eb9e4a8910ae549648

ADD evaluation/composition can be done with dict of subsitutions; ADD derivative/integral can be computed for given variable

src/UniVarPoly.py
@@ -121,8 +121,9 @@ class UniVarPoly:
 		for idx,c in enumerate(self.coeffs):
 			if isinstance(c, UniVarPoly):
 				c._normalize()
-				if c == 0:
-					self.coeffs[idx] = 0
+				if c.deg() == 0:
+					assert len(c.coeffs) <= 1
+					self.coeffs[idx] = 0 if len(c.coeffs) == 0 else c.coeffs[0]
 		if len(self.coeffs) == 1 and isinstance(self.coeffs[0], UniVarPoly):
 			self.varName = self.coeffs[0].varName
 			self.coeffs = self.coeffs[0].coeffs
@@ -519,11 +520,19 @@ class UniVarPoly:
 		'''
 		if isinstance(poly, numbers.Number):
 			return self.eval(poly)
-		poly_k = UniVarPoly(poly)
-		polyComp = UniVarPoly([self.coeffs[0]], varName=poly_k.varName)
+		if isinstance(poly, dict) and not self.varName in poly:
+			coeffsComp = [c.comp(poly) if isinstance(c, UniVarPoly) else c for c in self.coeffs]
+			return UniVarPoly(coeffsComp, varName=self.varName)
+		poly_var = UniVarPoly(poly[self.varName]) if isinstance(poly, dict) else UniVarPoly(poly)
+		poly_k = poly_var
+		def _subs(expr, s):
+			return expr.comp(s) if isinstance(expr, UniVarPoly) and isinstance(s, dict) else expr
+		c_0 = _subs(self.coeffs[0], poly)
+		polyComp = UniVarPoly([c_0], varName=poly_var.varName)
 		for k in range(1, len(self.coeffs)):
-			if k > 1: poly_k *= poly
-			polyComp += self.coeffs[k] * poly_k
+			if k > 1: poly_k *= poly_var
+			c_k = _subs(self.coeffs[k], poly)
+			polyComp += c_k * poly_k
 
 		polyComp._normalize()
 		return polyComp
@@ -557,12 +566,13 @@ class UniVarPoly:
 			
 			coeffsDer = [(i+1)*c for i,c in enumerate(self.coeffs[1:])]
 			return UniVarPoly(coeffsDer, varName=self.varName)
-		if varName > self.varName: return UniVarPoly(varName=varName)
+		if varName > self.varName:
+			return UniVarPoly(varName=varName)
 		coeffsDer = [c.der(varName) if isinstance(c, UniVarPoly) else 0 for c in self.coeffs]
 		return UniVarPoly(coeffsDer, varName=self.varName)
 
 
-	def int(self, interval=None):
+	def int(self, interval=None, varName=None):
 		'''compute indefinite or definite integral of polynomial
 		
 		   >>> p = UniVarPoly([0, 1])
@@ -572,32 +582,38 @@ class UniVarPoly:
 		   Fraction(1, 2)
 		'''
 		if interval is None:
-			return self.intIndef()
-		return self.intDef(interval)
+			return self.intIndef(varName)
+		return self.intDef(interval, varName)
 
 		
-	def intDef(self, interval):
+	def intDef(self, interval, varName=None):
 		'''definite integral.
 
 		   >>> p = UniVarPoly([0, 1])
 		   >>> p.intDef([0,1])
 		   Fraction(1, 2)
 		'''
-		pIntIndef = self.intIndef()
-		return pIntIndef.eval(interval[1]) - pIntIndef.eval(interval[0])
+		pIntIndef = self.intIndef(varName)
+		lower,upper = interval if varName is None else [{varName: val} for val in interval]
+		return pIntIndef.comp(upper) - pIntIndef.comp(lower)
 
 
-	def intIndef(self):
+	def intIndef(self, varName=None):
 		'''indefinite integral
 
 		   >>> p1 = UniVarPoly([1, 1])
 		   >>> p1.intIndef().coeffs
 		   [0, 1, Fraction(1, 2)]
 		'''
-		c0 = self.coeffs[0]
-		# make new coefficient (0) the same type as existing ones
-		# save division by 1
-		coeffsInt = [c0-c0, c0] + [self.coeffs[i]*Fraction(1,i+1) for i in range(1, len(self.coeffs))]
+		if varName is None or varName == self.varName:
+			c0 = self.coeffs[0]
+			# make new coefficient (0) the same type as existing ones
+			# save division by 1
+			coeffsInt = [c0-c0, c0] + [self.coeffs[i]*Fraction(1,i+1) for i in range(1, len(self.coeffs))]
+			return UniVarPoly(coeffsInt, varName=self.varName)
+		if varName > self.varName:
+			return UniVarPoly([0, self], varName=varName)
+		coeffsInt = [c.intIndef(varName) if isinstance(c, UniVarPoly) else UniVarPoly([0, c], varName=varName) for c in self.coeffs]
 		return UniVarPoly(coeffsInt, varName=self.varName)
 
 

examples/multiVar.py
@@ -8,6 +8,7 @@ import sys
 
 SCRIPT_DIR = os.path.split(os.path.abspath(__file__))[0]
 SRC_DIR = os.path.realpath(os.path.join(SCRIPT_DIR,'../src'))
+
 sys.path.append(SRC_DIR)
 from UniVarPoly import UniVarPoly as UP, symbol, symbols
 
@@ -23,31 +24,46 @@ print('p1:', p1,', p2:', p2)
 print('deg(p1):',      p1.deg(),   ', deg(p2):',      p2.deg())
 print('deg(p1, "x"):', p1.deg("x"),', deg(p1, "y"):', p1.deg("y"))
 print('deg(p2, "x"):', p2.deg("x"),', deg(p2, "y"):', p2.deg("y"))
+# composition
+print('p1(p2):', p1(p2), ', p2(p1):', p2(p1))
+print('p1(0):',  p1(0),  ', p1(1):',  p1(1))
+print('p1({x:1,y:2}):',  p1({'x':1,'y':2}), ', p2({x:y,y:2}):', p2({'x':1,'y':2}))
+print('p1({x:y}):',      p1({'x':y}),       ', p2({x:y}):',     p2({'x':y}))
 # derivative
 print('der(p1):',      p1.der(),   ', der(p2):',      p2.der())
 print('der(p1, "x"):', p1.der("x"),', der(p1, "y"):', p1.der("y"))
 print('der(p2, "x"):', p2.der("x"),', der(p2, "y"):', p2.der("y"))
+# indefinite integral
+print('int(p1):',      p1.intIndef(),   ', int(p2):',      p2.intIndef())
+print('int(p1, "x"):', p1.intIndef("x"),', int(p1, "y"):', p1.intIndef("y"))
+print('int(p2, "x"):', p2.intIndef("x"),', int(p2, "y"):', p2.intIndef("y"))
 
 p1p2 = p1+p2
+print('p1+p2: ', end='')
 dbgPoly(p1p2)
 print('p1:', p1,', p2:', p2)
 
 p2d1 = p2-p1
+print('p2-p1: ', end='')
 dbgPoly(p2d1)
 print('p1:', p1,', p2:', p2)
 
 p1d2 = p1-p2
+print('p1-p2: ', end='')
 dbgPoly(p1d2)
 print('p1:', p1,', p2:', p2)
 
 p0 = p1d2+p2d1
+print('(p2-p1) + (p1-p2): ', end='')
 dbgPoly(p0)
 print('equals 0:', p0 == 0)
 print('p1:', p1,', p2:', p2)
 
 p1m2 = p1*p2
+print('p1*p2: ', end='')
 dbgPoly(p1m2)
 print('p1:', p1,', p2:', p2)
+print('(p1*p2)({x:2}):',  p1m2({'x':2}), ', (p1*p2)({y:3}):', p1m2({'y':3}), ', (p1*p2)({x:2,y:3}):', p1m2({'x':2,'y':3}))
 
 z = symbol('z')
 ps = x+y+z

commit d33d8a5c8042781e7b26ec2e5ee7354ec5e45b49

FIX polynomial composition did not work for polynomials with degree >= 3 any more (bug introduced with commit 6d00a4070490c7c64243a5eb9e4a8910ae549648)

src/UniVarPoly.py
@@ -512,11 +512,14 @@ class UniVarPoly:
 	def comp(self, poly):
 		'''compute polynomial composition self o poly
 		
-		   >>> p1 = UniVarPoly([0,0,1])
-		   >>> p2 = UniVarPoly([-1,1])
-		   >>> p = p1.comp(p2)
+		   >>> p1 = UniVarPoly([0,0,1]) # x**2
+		   >>> p2 = UniVarPoly([-1,1])  # x-1
+		   >>> p = p1.comp(p2)          # (x-1)**2
 		   >>> p.coeffs
 		   [1, -2, 1]
+		   >>> p3 = UniVarPoly([0,0,0,1]) # x**3
+		   >>> p3.comp(p2).coeffs         # (x-1)**3
+		   [-1, 3, -3, 1]
 		'''
 		if isinstance(poly, numbers.Number):
 			return self.eval(poly)
@@ -525,7 +528,7 @@ class UniVarPoly:
 			return UniVarPoly(coeffsComp, varName=self.varName)
 		varName = poly.varName if isinstance(poly, UniVarPoly) else self.varName
 		poly_var = UniVarPoly(poly[self.varName], varName=varName) if isinstance(poly, dict) else UniVarPoly(poly, varName=varName)
-		poly_k = poly_var
+		poly_k = UniVarPoly(poly_var)
 		def _subs(expr, s):
 			return expr.comp(s) if isinstance(expr, UniVarPoly) and isinstance(s, dict) else expr
 		c_0 = _subs(self.coeffs[0], poly)
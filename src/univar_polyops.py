35f6262e378de92147e20d8605d8247bd70ee4bb
coproc <wolfgang@stoecher.com>
2020-12-13 16:26:27 +0100

    REF move definition of auxiliary function to defition of the only function using it

--- src/univar_polyops.py
+++ src/univar_polyops.py
@@ -8,12 +8,6 @@ e.g. as a list [c0, c1, ..., cn] representing the polynomial c0 + c1*x + ... + c
 from itertools import islice, zip_longest
 
 
-# islice does not allow a negative step
-def _rev_islice(cont, start, stop):
-	for i in range(start, stop, -1):
-		yield cont[i]
-
-
 def degree(coeffs):
 	"""highest power among monomials with non-zero coefficient; zero polynomial has degree -1"""
 	deg = len(coeffs) - 1
@@ -92,7 +86,13 @@ def multiply(coeffs1, coeffs2):
 	return coeffsMul
 
 
-# more compact/elegant, but slower
+# islice does not allow a negative step
+# (auxiliary function for multiply_v2)
+def _rev_islice(cont, start, stop):
+	for i in range(start, stop, -1):
+		yield cont[i]
+
+# more compact/elegant than multiply, but slower
 def multiply_v2(coeffs1, coeffs2):
 	deg1,deg2 = degree(coeffs1), degree(coeffs2)
 	_scal_prod = lambda v1,v2: sum(c1*c2 for c1,c2 in zip(v1,v2))
